4ba0b6ebf959347ca89d279c169ad28802987a51
gonzalojpv <gonzalojpv@gmail.com>
2017-03-31 13:19:56 -0600

    GP, setup

---
+++ src/Todos/Middleware/Authentication.php
@@ -0,0 +1,27 @@
+<?php
+namespace Todos\Middleware;
+use Todos\Models\User;
+
+/**
+ *
+ */
+class Authentication
+{
+  public function authenticate( $request, $app ) {
+
+    $auth = $request->headers->get("Authorization");
+    $apikey = substr($auth, strpos($auth, ' '));
+    $apikey = trim($apikey);
+    $user = new User();
+    $check = $user->authenticate($apikey);
+
+    if ( !$check )
+      $app->abort(401);
+    else {
+      $request->atrributes->set( 'userid', $check );
+    }
+
+  }
+}
+
+?>
