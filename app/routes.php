31f0c7d15f6f409e7531b7cdbd8d231017ae5e22
gonzalojpv <gonzalojpv@gmail.com>
2017-04-04 19:50:27 -0500

    GP, message provider

--- app/routes.php
+++ app/routes.php
@@ -1,3 +1,4 @@
 <?php
 $app->mount("/users", new Todos\Provider\UserProvider());
+$app->mount("/messages", new Todos\Provider\MessageProvider());
 ?>
---
+++ src/Todos/Controller/Message.php
@@ -0,0 +1,29 @@
+<?php
+namespace Todos\Controller;
+/**
+ *
+ */
+class Message {
+
+  public static function edit($id){
+    // show edit form
+  }
+
+  public static function show($id){
+    // show the message #id
+  }
+
+  public static function store(){
+    // create a new message, using POST method
+  }
+
+  public static function update($id){
+    // update the message #id, using PUT method
+  }
+
+  public static function destroy($id){
+    // delete the message #id, using DELETE method
+  }
+}
+
+?>
