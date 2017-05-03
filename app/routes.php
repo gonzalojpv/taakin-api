5d7340a6e71702c9c9d80475a4772e0ff7e822fe
gonzalojpv <gonzalojpv@gmail.com>
2017-05-03 01:37:21 -0500

    GP, Customer: Entity

--- app/routes.php
+++ app/routes.php
@@ -1,4 +1,5 @@
 <?php
 $app->mount("/users", new Taakin\Provider\UserProvider());
 $app->mount("/amounts", new Taakin\Provider\AmountProvider());
+$app->mount("/customers", new Taakin\Provider\CustomerProvider());
 ?>
--- src/Taakin/Controller/User.php
+++ src/Taakin/Controller/User.php
@@ -6,23 +6,23 @@ namespace Taakin\Controller;
  */
 class User {
 
-  public static function edit($id){
+  public static function edit( $id ) {
     // show edit form
   }
 
-  public static function show($id){
+  public static function show( $id ) {
     // show the user #id
   }
 
-  public static function store(){
+  public static function store() {
     // create a new user, using POST method
   }
 
-  public static function update($id){
+  public static function update( $id ) {
     // update the user #id, using PUT method
   }
 
-  public static function destroy($id){
+  public static function destroy( $id ) {
     // delete the user #id, using DELETE method
   }
 }
