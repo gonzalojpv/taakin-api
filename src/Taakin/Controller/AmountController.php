37b9a72adf4ac2ce25c8cf67d6c82cb599478f4c
gonzalojpv <gonzalojpv@gmail.com>
2017-07-06 19:37:28 -0500

    GP, add field code to amounts table

--- src/Taakin/Controller/AmountController.php
+++ src/Taakin/Controller/AmountController.php
@@ -40,6 +40,7 @@ class AmountController {
     $amount = new Amount();
     $amount->amount = $request->get('amount');
     $amount->customer_id = $request->get('customer_id');
+    $amount->code = self::getToken(10);
 
     if ( $amount->save() ) {
       $payload = ['amount_id' => $amount->id, 'amount_uri' => '/amounts/' . $amount->id];
@@ -81,6 +82,34 @@ class AmountController {
       return new Response( '', 400 );
     else
       return new Response( '', 204 );
+  }
+
+  private static function crypto_rand_secure( $min, $max ) {
+    $range = $max - $min;
+    if ($range < 1) return $min; // not so random...
+    $log = ceil(log($range, 2));
+    $bytes = (int) ($log / 8) + 1; // length in bytes
+    $bits = (int) $log + 1; // length in bits
+    $filter = (int) (1 << $bits) - 1; // set all lower bits to 1
+    do {
+        $rnd = hexdec(bin2hex(openssl_random_pseudo_bytes($bytes)));
+        $rnd = $rnd & $filter; // discard irrelevant bits
+    } while ($rnd > $range);
+    return $min + $rnd;
+  }
+
+  private static function getToken( $length ) {
+    $token = "";
+    $codeAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $codeAlphabet.= "abcdefghijklmnopqrstuvwxyz";
+    $codeAlphabet.= "0123456789";
+    $max = strlen($codeAlphabet); // edited
+
+    for ($i=0; $i < $length; $i++) {
+        $token .= $codeAlphabet[self::crypto_rand_secure(0, $max-1)];
+    }
+
+    return $token;
   }
 }
 
